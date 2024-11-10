6120dda38bab95b75579ca35d85c305593e90d92
Cho R <rammenta@gmail.com>
2024-11-11 06:59:38 +0900

    new file:   js.html 	new file:   jstest.js

---
+++ jstest.js
@@ -0,0 +1,36 @@
+/*
+
+*/
+
+var numplate = 10;
+var undf;
+/* undefined 변수 선언 */
+
+undf="변수 초기화";
+
+var undf = "변수의 선언과 초기화를 함께";
+
+console.log(undf);
+
+// 변수에 사용 불가한 단어 : 영어 외의 언어 불가, 숫자가 앞에 오는 경우 불가. 특문도 불가(-,_ 제외?)
+
+// 예약어(JS Reserved word / JS keyword 검색) 프로그래밍 언어가 자체적으로 사용 할 단어나 키워드는 변수로 선언할 수 없음 
+
+// 변수명(식별자)
+// 1. camel Case
+// var sendemail 
+// 을 sendEmailDate (첫번째 단어의 첫글자는 소문자나 그 후 단어의 첫번째 단어는 대문자) 주로 이게 사용됨
+
+
+// 2. snake_case
+// 전체가 소문자고 언더바로 이어짐
+// var send_email_data
+
+// 주석
+// 1. 싱글라인은 //
+/* 
+
+2. 멀티라인
+주석
+
+*/
